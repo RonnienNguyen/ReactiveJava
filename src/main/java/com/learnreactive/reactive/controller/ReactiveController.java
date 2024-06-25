d989c36282aeccaf85783750ae895c967923d882
Your Name <your_email@example.com>
2024-06-25 14:26:55 +0700

    Origin

--- src/main/java/com/learnreactive/reactive/controller/ReactiveController.java
+++ src/main/java/com/learnreactive/reactive/controller/ReactiveController.java
@@ -13,32 +13,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/items")
+@RequestMapping("/api")
 public class ReactiveController {
 
     @Autowired
     private ItemService itemService;
 
-    @GetMapping("/")
+    @GetMapping("/items")
     public Flux<String> getAllItems() {
         return Flux.just("Item 1", "Item 2", "Item 3");
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/items/{id}")
     public Mono<ResponseEntity<String>> getItemById (@PathVariable String id) {
         return Mono.just("Item" + id)
                 .map(item -> ResponseEntity.ok(item))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/getAll")
+    @GetMapping("/items/getAll")
     public Flux<ResponseEntity<Item>> getAll() {
         return itemService.getAllItems()
-                .map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build()); // Handle empty case if needed
+                .map(ResponseEntity::ok);
     }
 
-    @PostMapping("/")
+    @PostMapping("/items")
     public Mono<ResponseEntity<Object>> createItem(@RequestBody String newItem) {
         // Create a map to hold the response data
         Map<String, Object> responseJson = new HashMap<>();
